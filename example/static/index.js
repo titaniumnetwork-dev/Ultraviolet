05d943a8363e2476b27c869605acb1433cc67ffe
Jason <59297610+caracal-js@users.noreply.github.com>
2022-02-23 18:25:59 -0500

    Example moved

--- example/static/index.js
+++
@@ -1,6 +0,0 @@
-importScripts('./uv.bundle.js');
-importScripts('./uv.handler.js');
-
-__uv.client.location.overrideWorkerLocation(() => new URL('https://www.google.com'));
-
-console.log(postMessage.__uv$string);
