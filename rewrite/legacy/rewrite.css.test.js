94fd8d34758baa6e93ebd485fea2af7646cbbc1a
Caracal.js <59297610+caracal-js@users.noreply.github.com>
2022-03-13 00:27:30 -0500

    Deleted legacy files

--- rewrite/legacy/rewrite.css.test.js
+++
@@ -1,9 +0,0 @@
-function url(ctx, meta = ctx.meta) {
-    const { css } = ctx;
-
-    css.on('url', event => {
-        event.value = ctx.rewriteUrl(event.value, meta);
-    });
-};
-
-export { url };
