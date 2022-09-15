471f591ab02b68c1f2dbaa54f0e73d49c241944f
David Reed <reedswork@proton.me>
2022-09-15 17:12:58 -0400

    import core scripts from ultraviolet-core

---
+++ src/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/service/',
+    bare: 'https://incog.dev/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv.handler.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
+};
