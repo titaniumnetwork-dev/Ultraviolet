8525734527ebd7d0560d63bdd4377d3846197943
David Reed <reedswork@proton.me>
2022-09-15 17:36:42 -0400

    bundle & copy files, provide an API for using a packaged version of ultraviolet

---
+++ lib/index.d.ts
@@ -0,0 +1,1 @@
+export const uvPath: string;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 import webpack from 'webpack';
 import { fileURLToPath } from 'url';
+import TerserPlugin from 'terser-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+import { resolve } from 'path';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -8,11 +11,32 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
  */
 const config = {
 	mode: isDevelopment ? 'development' : 'production',
-	entry: fileURLToPath(new URL('./rewrite/index.js', import.meta.url)),
-	output: {
-		path: fileURLToPath(new URL('./lib/', import.meta.url)),
-		filename: 'uv.bundle.js',
+	entry: {
+		bundle: fileURLToPath(new URL('./src/rewrite/index.js', import.meta.url)),
+		handler: fileURLToPath(new URL('./src/uv.handler.js', import.meta.url)),
+		sw: fileURLToPath(new URL('./src/uv.sw.js', import.meta.url)),
 	},
+	output: {
+		path: fileURLToPath(new URL('./dist/', import.meta.url)),
+		filename: 'uv.[name].js',
+	},
+	optimization: {
+		minimize: !isDevelopment,
+		minimizer: [
+			new TerserPlugin({
+				exclude: ['uv.config.js'],
+			}),
+		],
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{
+					from: fileURLToPath(new URL('./src/uv.config.js', import.meta.url)),
+				},
+			],
+		}),
+	],
 	performance: {
 		// suppress "entrypoint size limit" warning
 		hints: false,
