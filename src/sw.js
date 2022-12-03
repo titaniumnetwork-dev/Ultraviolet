1fd66c00fa6fddc9ae7af1a5fa79ed2b102ef284
David Reed <reedswork@proton.me>
2022-12-03 15:55:39 -0500

    use sw from config

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-/*global UVServiceWorker*/
+/*global UVServiceWorker,__uv$config*/
 /*
  * Stock service worker script.
  * Users can provide their own sw.js if they need to extend the functionality of the service worker.
@@ -7,7 +7,7 @@
  */
 importScripts('uv.bundle.js');
 importScripts('uv.config.js');
-importScripts('uv.sw.js');
+importScripts(__uv$config.sw || 'uv.sw.js');
 
 const sw = new UVServiceWorker();
 
