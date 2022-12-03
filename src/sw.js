9f60e73b5a7da8bb530fcbd0e0f5976aa2371b1d
David Reed <reedswork@proton.me>
2022-12-03 15:54:45 -0500

    add note

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,10 @@
 /*global UVServiceWorker*/
+/*
+ * Stock service worker script.
+ * Users can provide their own sw.js if they need to extend the functionality of the service worker.
+ * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
+ * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
+ */
 importScripts('uv.bundle.js');
 importScripts('uv.config.js');
 importScripts('uv.sw.js');
