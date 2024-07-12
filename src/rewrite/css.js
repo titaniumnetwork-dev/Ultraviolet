51b71aa53c4eb682716b263ca40be120b106c62e
Avad3 <65318266+Avad3@users.noreply.github.com>
2024-07-12 00:48:38 -0400

    fix css rewriting (#150)

--- src/rewrite/css.js
+++ src/rewrite/css.js
@@ -16,7 +16,12 @@ class CSS extends EventEmitter {
     recast(str, options, type) {
         if (!str) return str;
         str = new String(str).toString();
-        // no rewriting rn cry ab it
+        str = str.replace(/(?<=url\("?'?)[^"'][\S]*[^"'](?="?'?\);?)/gm, (match) => {
+            return type === "rewrite" ? this.ctx.rewriteUrl(match) : this.ctx.sourceUrl(match);
+        });
+        str = str.replace(/@import\s+(['"])?([^'"\);]+)\1?\s*(?:;|$)/gm, (match, quote, url) => {
+            return `@import ${quote || ""}${type === "rewrite" ? this.ctx.rewriteUrl(url) : this.ctx.sourceUrl(url)}${quote || ""};`;
+        });
         return str;
     }
 }
