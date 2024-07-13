f35903c8df895da50896e5600b0cd22a262d80fb
Percs <83934299+Percslol@users.noreply.github.com>
2024-07-12 23:55:21 -0500

    clean up sw request handling

--- src/sw.js
+++ src/sw.js
@@ -11,13 +11,14 @@ importScripts(__uv$config.sw || 'uv.sw.js');
 
 const uv = new UVServiceWorker();
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        (async ()=>{
-            if (uv.route(event)) {
-                return await uv.fetch(event);
-            }
-            return await fetch(event.request);
-        })()
-    );
+async function handleRequest(event) {
+    if (uv.route(event)) {
+        return await uv.fetch(event);
+    }
+    
+    return await fetch(event.request)
+}
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event));
 });
