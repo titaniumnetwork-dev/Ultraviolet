18376578e9704d104c8ef93b154e87544f59760d
Percs <83934299+Percslol@users.noreply.github.com>
2024-08-01 04:18:14 -0500

    update xor functions (credit @theogbob)

--- src/rewrite/codecs.js
+++ src/rewrite/codecs.js
@@ -4,29 +4,23 @@
 // -------------------------------------------------------------
 export const xor = {
     encode(str) {
-        if (!str) return str;
-        return encodeURIComponent(
-            str
-                .toString()
-                .split('')
-                .map((char, ind) =>
-                    ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
-                )
-                .join('')
-        );
+      if (!str) return str;
+      let result = "";
+      for (let i = 0; i < str.length; i++) {
+        result += i % 2 ? String.fromCharCode(str.charCodeAt(i) ^ 2) : str[i];
+      }
+      return encodeURIComponent(result);
     },
     decode(str) {
-        if (!str) return str;
-        let [input, ...search] = str.split('?');
-
-        return (
-            decodeURIComponent(input)
-                .split('')
-                .map((char, ind) =>
-                    ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char
-                )
-                .join('') + (search.length ? '?' + search.join('?') : '')
-        );
+      if (!str) return str;
+      const [input, ...search] = str.split("?");
+      let result = "";
+      const decoded = decodeURIComponent(input);
+      for (let i = 0; i < decoded.length; i++) {
+        result +=
+          i % 2 ? String.fromCharCode(decoded.charCodeAt(i) ^ 2) : decoded[i];
+      }
+      return result + (search.length ? "?" + search.join("?") : "");
     },
 };
 
