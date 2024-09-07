ea22514958a0f7aa998cb53327c95c9a33920a68
Percs <83934299+Percslol@users.noreply.github.com>
2024-09-07 17:22:39 -0500

    revert to meteor css rewriter temporarily

--- src/rewrite/css.js
+++ src/rewrite/css.js
@@ -14,35 +14,35 @@ class CSS extends EventEmitter {
         return this.recast(str, options, 'source');
     }
     recast(str, options, type) {
-        // credit to (https://github.com/callumlocke/css-url-rewriter)
-        const cssPropertyMatcher = /@import[^;]*|[;\s{]?\*?[a-zA-Z\-]+\s*:#?[\s\S]*url\(\s*['"]?[^'"\)\s]+['"]?\s*\)[^;}]*/g;
-        const urlMatcher = /url\(\s*['"]?([^)'"]+)['"]?\s*\)/g;
+        // CSS Rewriting from Meteor (https://github.com/meteorproxy/meteor)
+        const regex =
+        /(@import\s+(?!url\())?\s*url\(\s*(['"]?)([^'")]+)\2\s*\)|@import\s+(['"])([^'"]+)\4/g
 
-        const settings = {
-            excludeProperties: ['behavior', '*behavior']
-          };
-
-        str = str.toString().replace(cssPropertyMatcher, (property) => {
-            // This function deals with an individual CSS property.
-
-            // If this property is excluded, return it unchanged
-            if (settings.excludeProperties.length) {
-            const propertyName = property.split(':')[0].replace(/^\s+|\s+$/g, '');
-
-                for (let i = settings.excludeProperties.length - 1; i >= 0; i--) {
-                    if (propertyName.indexOf(settings.excludeProperties[i]) === 0) {
-                        return property;
-                    }
+        if (!str) return str;
+        str = new String(str).toString();
+        return str.replace(
+            regex,
+            (
+                match,
+                importStatement,
+                urlQuote,
+                urlContent,
+                importQuote,
+                importContent
+            ) => {
+                const url = urlContent || importContent
+                const encodedUrl = type === "rewrite" ? this.ctx.rewriteUrl(url) : this.ctx.sourceUrl(url);
+    
+                if (importStatement) {
+                    return `@import url(${urlQuote}${encodedUrl}${urlQuote})`
                 }
-            }
-
-            // Return the property with the URL rewritten
-            return property.replace(urlMatcher, (urlFunc, url) => {
-                return urlFunc.replace(url, type === "rewrite" ? this.ctx.rewriteUrl(url) : this.ctx.sourceUrl(url));
-            });
+    
+                if (importQuote) {
+                    return `@import ${importQuote}${encodedUrl}${importQuote}`
+                }
+    
+                return `url(${urlQuote}${encodedUrl}${urlQuote})`
         });
-
-        return str;
     }
 }
 
