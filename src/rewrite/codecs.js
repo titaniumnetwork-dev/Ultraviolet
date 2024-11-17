b1e9c323e1b64c60283976f061f1be6c92cafc54
theogbob <168785618+theogbob@users.noreply.github.com>
2024-11-16 23:07:08 -0500

    Update codecs.js (#167)
    Even faster xor functions by about 2.1x in testing, same output as before so no breaking changes but it could be even faster if it wasent forced to be the same output:
    
    > https://gist.github.com/theogbob/89bfd228d7ec646bac14db867f33b8b2

--- src/rewrite/codecs.js
+++ src/rewrite/codecs.js
@@ -21,23 +21,25 @@ export const plain = {
 export const xor = {
 	encode(str) {
 		if (!str) return str;
-		let result = "";
-		for (let i = 0; i < str.length; i++) {
-			result += i % 2 ? String.fromCharCode(str.charCodeAt(i) ^ 2) : str[i];
-		}
-		return encodeURIComponent(result);
+	        let result = "";
+	        let len = str.length;
+	        for (let i = 0; i < len; i++) {
+	            const char = str[i];
+	            result += i % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char;
+	        }
+	        return encodeURIComponent(result);
 	},
 	decode(str) {
-		if (!str) return str;
-		const [input, ...search] = str.split("?");
-		let result = "";
-		const decoded = decodeURIComponent(input);
-		for (let i = 0; i < decoded.length; i++) {
-			result +=
-				i % 2 ? String.fromCharCode(decoded.charCodeAt(i) ^ 2) : decoded[i];
-		}
-		return result + (search.length ? "?" + search.join("?") : "");
-	},
+	        if (!str) return str;
+	        str = decodeURIComponent(str);
+	        let result = "";
+	        let len = str.length;
+	        for (let i = 0; i < len; i++) {
+	            const char = str[i];
+	            result += i % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char;
+	        }
+		return result;
+	}
 };
 
 export const base64 = {
